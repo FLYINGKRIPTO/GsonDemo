d3d33281fe0ced9d85dbeaae3785a671f5627c32
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-10-05 14:24:54 +0530

    Gson Implementation

--- app/src/main/java/com/sriyanksiddhartha/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/sriyanksiddhartha/sharedpreferences/MainActivity.java
@@ -1,9 +1,16 @@
 package com.sriyanksiddhartha.sharedpreferences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.util.Arrays;
 
 /**
  * Author : Sriyank Siddhartha
@@ -27,17 +34,58 @@ public class MainActivity extends AppCompatActivity {
 
 	public void saveObjectType(View view) {
 
+	    Employee employee = getEmployeeObject();
+		SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = sharedPreferences.edit();
+		//Serialization by GSON Library
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(employee,Employee.class);
+        Log.i(TAG, jsonString);
+
+        editor.putString("employee_key",jsonString);
+        editor.apply();
+
 	}
 
 	public void loadObjectType(View view) {
+	    SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+	    String jsonString = sharedPreferences.getString("employee_key","N/A");
+        Log.i(TAG,jsonString);
+
+        //Deserialization
+        Gson gson = new Gson();
+        Employee employee = gson.fromJson(jsonString,Employee.class);
+        displayText(employee);
+
+
 
 	}
 
-	public void saveGenericType(View view) {
+    private void displayText(Employee employee) {
+	    if(employee==null)
+        {
+            return;
+        }
+	    String displayText = employee.getName()+
+                "\n" + employee.getProfession()
+	           +"\n"+employee.getProfId()
+                +"\n"+employee.getRoles();
+	    txvDisplay.setText(displayText);
+    }
+
+    public void saveGenericType(View view) {
 
 	}
 
 	public void loadGenericType(View view) {
 
 	}
+	private Employee getEmployeeObject(){
+        Employee employee = new Employee();
+        employee.setName("Shivanshu Srivastava");
+        employee.setProfession("Android Developer");
+        employee.setProfId(287);
+        employee.setRoles(Arrays.asList("Developer","Admin"));
+        return employee;
+    }
 }
